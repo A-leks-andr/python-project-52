2644b391e9631a1b3dc95476181d8476785a676d
A-leks-andr <calllamurzakov@gmail.com>
2026-01-10 21:11:44 +0300

    fix files and add files users

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,5 +5,6 @@ from task_manager import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("users/", views.UsersView.as_view(), name="users"),
     path("admin/", admin.site.urls),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    def get(self, request, *args, **kwargs):
+        return render(request, "index.html")
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["who"] = "World"
-        return context
+class UsersView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, "users.html")
