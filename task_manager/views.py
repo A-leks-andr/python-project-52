a8b93931614884902aa03b8f37e398b175666808
A-leks-andr <calllamurzakov@gmail.com>
2026-01-13 18:25:46 +0300

    fixed 3 files

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+menu_unregistered = [
+    {'url_name': 'users', 'name': 'Пользователи'},
+    {'url_name': 'login', 'name': 'Вход'},
+    {'url_name': 'create', 'name': 'Регистрация'},
+]
 
 class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, "index.html")
+        data = {'menu': menu_unregistered}
+        return render(request, "index.html", context=data)
 
 
 class UsersView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, "users.html")
-
+        data = {'menu': menu_unregistered}
+        return render(request, "users.html", context=data)
 
 class LoginView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, "login.html")
+        data = {'menu': menu_unregistered}
+        return render(request, "login.html", context=data)
 
 
 class CreateView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, "create.html")
+        data = {'menu': menu_unregistered}
+        return render(request, "create.html", context=data)
