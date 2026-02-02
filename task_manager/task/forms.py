73cfb17f662aa57b303bc2f582459c924bdd88ff
A-leks-andr <calllamurzakov@gmail.com>
2026-02-02 05:01:01 +0300

    fixed for test

--- task_manager/task/forms.py
+++ task_manager/task/forms.py
@@ -34,7 +34,7 @@ class CreateTaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = ["name", "description", "status", "executor", "label"]
+        fields = ["name", "description", "status", "executor", "labels"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
